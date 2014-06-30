a441d343066446e3b3f469ec515742ba817b6721
Shane Sprague <shanesprague89@gmail.com>
2014-06-30 13:21:10 -0400

    config serve status assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,9 +23,18 @@ SampleApp::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  
+  
+  
+  
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # was false changed to true 6/30/2014
+  config.serve_static_assets = true
 
+  
+  
+  
+  
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
