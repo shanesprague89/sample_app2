ead24404d5a988c83ec4d423bac6f52d8eb586ed
Shane Sprague <shanesprague89@gmail.com>
2014-07-05 10:09:49 -0400

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,26 @@ class User < ActiveRecord::Base
     before_save { email.downcase! } #lowercases before saving to database.
                                     #Not all databases are case sensitive.
     
+    before_create :create_remember_token
+    
+    def User.new_remember_token
+        SecureRandom.urlsafe_base64
+    end
+    
+    def User.digest(token)
+        Digest::SHA1.hexdigest(token.to_s)
+    end
+    
     validates :name,    presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email,   presence: true, format: { with: VALID_EMAIL_REGEX },
                         uniqueness: { case_sensitive: false }
                         
     validates :password, length: { minimum: 6 }
+    
+    private
+    
+        def create_remember_token
+            self.remember_token = User.digest(User.new_remember_token)
+        end
 end
